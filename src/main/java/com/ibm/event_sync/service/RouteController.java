584ae5d6ec1a83fe7e9890736a3a2499e669e27d
Gerda Tumelytė <gertum1@ktu.lt>
2025-07-11 04:52:50 +0300

    api and web controller separation

--- src/main/java/com/ibm/event_sync/service/RouteController.java
+++ src/main/java/com/ibm/event_sync/service/RouteController.java
@@ -1,4 +1,4 @@
-package com.ibm.event_sync.controllers;
+package com.ibm.event_sync.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SimpleController {
+public class RouteController {
     @Value("${spring.application.name}")
     String appName;
 
